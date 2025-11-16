f43b4910acee691cb9ba9682d22193f2a3c628e2
sudo29 <sripadgarodia@gmail.com>
2025-11-16 17:51:42 +0700

    Add inventory admin interfaces (InventoryBatch, InventorySample, InventoryScanLog)

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Order, ImageAttachment, ScanLog
+from .models import Product, Order, ImageAttachment, ScanL, InventoryBatch, InventorySample, InventoryScanLog
 
 
 @admin.register(Product)
